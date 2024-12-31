b54b20f34925e288dd5fe2086a3e82156d428963
bijoymandal <bijoymandal00@gmail.com>
2024-12-31 05:56:02 +0530

    todo list add using object

--- src/projects/Todo/TodoForm.jsx
+++ src/projects/Todo/TodoForm.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export const TodoForm = ({onAddTodo}) => {
 
     //Todo props function validation
-    const [inputValue,setInputValue]= useState(""); //initial state , updated state
+    const [inputValue,setInputValue]= useState({}); //initial state , updated state
     // console.log(onAddTodo);
     TodoForm.propTypes = {
         onAddTodo : PropTypes.func.isRequired
@@ -13,14 +13,14 @@ export const TodoForm = ({onAddTodo}) => {
     
 
     const handleInputChange = (value) =>{
-        setInputValue(value);
+        setInputValue({id:value,content:value,checked:false});
     }
 
     const handleFormSubmit = (event) =>{
         event.preventDefault();
         onAddTodo(inputValue);
         console.log(inputValue);
-        setInputValue("");
+        setInputValue({id:"",content:"",checked:false});
     }
 
     
@@ -31,7 +31,7 @@ export const TodoForm = ({onAddTodo}) => {
                                 <form onSubmit={handleFormSubmit}>
                                     <div>
                                         <input type="text" className="todo-input border pl-3 border-gray-900 rounded-full focus:text-gray-500
-                                        " autoComplete="off" value={inputValue} onChange={(event)=>handleInputChange(event.target.value)}/>
+                                        " autoComplete="off" value={inputValue.content} onChange={(event)=>handleInputChange(event.target.value)}/>
                                     </div>
                                     <div>
                                         <button type="submit" className="bg-green-700 text-center text-white dark:text-gray-700 px-3 py-1 rounded-full border-1 border-slate-700">Add Task</button>
