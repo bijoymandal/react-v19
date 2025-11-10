e72a0f596feb96eef73c983a3f6c90800f145f8f
bijoymandal <bijoy.mandal@mydevteam.com>
2025-11-10 18:09:15 +0530

    all component usages as a main "app.js "

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import Footer,{ NetflixCode } from "./components/NetflixSeries";
 // import { Pratices } from "./Pratices";
 
-import UseEffect from "./components/hooks/useEffect" 
-import PokeMonAPI from "./Pokemon/PokemonAPI"
+// import UseEffect from "./components/hooks/useEffect"
+// import PokeMonAPI from "./Pokemon/PokemonAPI"
 // import { Challenge } from "./components/hooks/useState/Challenge"
+// import Registration from "./components/hooks/useState/Registration"
+import { UseRef } from "./components/hooks/useRef";
 // import { Controlled } from "./components/hooks/useState/controlled"
 // import { ShortCircuit } from "./components/ShortCircuit"
 // import { Todo } from "./projects/Todo/Todo"
@@ -12,22 +14,24 @@ import PokeMonAPI from "./Pokemon/PokemonAPI"
 function App() {
 
   return (
-    <>  
-        {/* React Fragment */}
-        This is used React Js and Javascript Projects
-        {/* React Component  */}
-        {/* <NetflixCode/>    */}
-        {/* <Pratices /> */}
-        {/* <Footer/> */}
+    <>
+      {/* React Fragment */}
+      This is used React Js and Javascript Projects
+      {/* React Component  */}
+      {/* <NetflixCode/>    */}
+      {/* <Pratices /> */}
+      {/* <Footer/> */}
 
-        {/* <ToggleSwitch/> */}
-        {/* <Todo/> */}
-        {/* <ShortCircuit/> */}
-        {/* <Challenge/> */}
-        {/* <Controlled/> */}
-        {/* <UseEffect/> */}
-        {/* <PokeMonAPI/>  */}
-        <PokeMonAPI/>
+      {/* <ToggleSwitch/> */}
+      {/* <Todo /> */}
+      {/* <ShortCircuit/> */}
+      {/* <Challenge /> */}
+      {/* <Controlled/> */}
+      {/* <UseEffect/> */}
+      {/* <PokeMonAPI/>  */}
+      {/* <PokeMonAPI/> */}
+      {/* <Registration /> */}
+      <UseRef />
     </>
   )
 }
